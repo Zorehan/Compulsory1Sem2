95596af01ce228f45ae0302343e50737ebfaa25b
Gergö Kadar <kadargergo20@gmail.com>
2024-02-20 13:44:16 +0100

    quality of life css ændringer

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         // primaryStage.setFullScreen(true);
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/login.css").toExternalForm());
-        scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;400;500;600;700;800;900&display=swap");
+        scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;400;500;600;700;800;900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/dk/easv/presentation/controller/LogInController.java
+++ src/dk/easv/presentation/controller/LogInController.java
@@ -37,7 +37,7 @@ public class LogInController implements Initializable {
                 Stage stage = new Stage();
                 Scene sceneMainWindow = new Scene(root);
                 sceneMainWindow.getStylesheets().add(getClass().getResource("/mainWindow.css").toExternalForm());
-                sceneMainWindow.getStylesheets().add("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;400;500;600;700;800;900&display=swap");
+                sceneMainWindow.getStylesheets().add("https://fonts.googleapis.com/css2?family=Inknut+Antiqua:wght@300;400;500;600;700;800;900&family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
                 stage.setScene(sceneMainWindow);
                 stage.setTitle("Movie Recommendation System 0.01 Beta");
                 stage.show();
